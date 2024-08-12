655b0f462e45faa3abc8b8a12519b4f7083a540e
ttobe <ahdrmfgur12@gmail.com>
2024-08-12 13:36:39 +0900

    fix: 결과가 같은 경우에 빨간 테두리 추가 [#42]

--- views/javascript/spell/popup.js
+++ views/javascript/spell/popup.js
@@ -2,7 +2,7 @@
 import { spellCheck } from './spellCheck.js';
 
 const diffTitle = `교정된 결과가 맞는지 확인해주세요.<br>직접 수정할 수 있어요!`;
-const sameTitle = `교정된 결과가 없는 입력입니다.<br>직접 수정해주세요!`;
+const sameTitle = `맞춤법을 찾을 수 없어요.<br>직접 수정해주세요!`;
 
 export function showSuggestion(event, element, idx) {
   event.stopPropagation(); // 이벤트 전파 막기
@@ -19,8 +19,8 @@ function showPopup(title, element, idx) {
     `
       <div class="suggestion-edit-container">
         <div class="suggestion-edit-instructions">${element.innerText}</div>
-        <input type="text" id="suggestion-edit" class="suggestion-edit" value="${element.getAttribute('data-suggestions')}">
-      </div>
+        <input type="text" id="suggestion-edit" value="${element.getAttribute('data-suggestions')}">
+      </div>  
     `,
     () => {
       const editedSuggestion = document.getElementById('suggestion-edit').value;
@@ -32,6 +32,9 @@ function showPopup(title, element, idx) {
       outputPopup.hide();
     },
   );
+  if (title === sameTitle) {
+    document.getElementById('suggestion-edit').classList.add('wrong');
+  }
 
   outputPopup.show();
 }
