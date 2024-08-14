f8c28bcb82651f87af69e9520d75229c365a114f
kimyu0218 <serena35@ewhain.net>
2024-08-15 04:42:22 +0900

    refactor: import 경로 단순화를 위한 index 파일 작성

--- src/clova/constants/index.ts
+++ src/clova/constants/index.ts
@@ -2,3 +2,4 @@ export * from './cloav-request-headers-details';
 export * from './clova-request-body-details';
 export * from './feedback';
 export * from './partial-modification';
+export * from './stream-request-header';
--- src/clova/types/clova-chat-completions/index.ts
+++ src/clova/types/clova-chat-completions/index.ts
@@ -1,3 +1,4 @@
 export * from './chat-message.type';
 export * from './request-body.type';
 export * from './response-body.type';
+export * from './stream-response-body.type';
