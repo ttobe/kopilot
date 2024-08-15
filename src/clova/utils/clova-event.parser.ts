014a7983316d864177609c02ff356c297a11a696
kimyu0218 <serena35@ewhain.net>
2024-08-15 14:32:15 +0900

    fix: JSON 파싱 오류 수정 [#99]
    - 기존 정규식이 중첩된 중괄호를 제대로 처리하지 못함
    - 다음 토큰 시작을 알리는 플래그가 오기 전까지 모든 data를 포함하도록
      변경

--- src/clova/utils/clova-event.parser.ts
+++ src/clova/utils/clova-event.parser.ts
@@ -7,7 +7,7 @@ import {
 
 export class ClovaEventParser {
   private static EVENT_PATTERN =
-    /id:(?<id>[^\n]+)\nevent:(?<event>[^\n]+)\ndata:(\n)*(?<data>\{.*\})/;
+    /id:(?<id>[^\n]+)\nevent:(?<event>[^\n]+)\ndata:(\n)*(?<data>\{.*)(?!\nid:)/;
 
   private buffer: string = '';
 
@@ -34,8 +34,10 @@ export class ClovaEventParser {
     singleEvent: string;
     remainingBuffer: string;
   } {
-    const match = ClovaEventParser.EVENT_PATTERN.exec(this.buffer);
-    if (!match || !match.groups) {
+    const match: RegExpExecArray | null = ClovaEventParser.EVENT_PATTERN.exec(
+      this.buffer,
+    );
+    if (!this.isValidEvent(match)) {
       throw new Error('Failed to extract single event');
     }
 
@@ -47,21 +49,33 @@ export class ClovaEventParser {
   }
 
   private parseSingleEvent(singleEvent: string): ClovaStreamToken | null {
-    const match = singleEvent.match(ClovaEventParser.EVENT_PATTERN);
+    const match: RegExpMatchArray | null = singleEvent.match(
+      ClovaEventParser.EVENT_PATTERN,
+    );
 
-    if (!match || !match.groups) {
-      console.error('Failed to parse single event:', singleEvent);
+    if (!this.isValidEvent(match)) {
+      console.error(`Failed to parse single event: ${singleEvent}`);
       return null;
     }
 
     const { event, data } = match.groups;
-    switch (event as ClovaEvent) {
-      case 'token':
-        return JSON.parse(data) as ClovaChatCompletionsStreamToken;
-      case 'signal':
-        return JSON.parse(data) as ClovaChatCompletionsStreamSignal;
-      default:
-        return null;
+    try {
+      switch (event as ClovaEvent) {
+        case 'token':
+          return JSON.parse(data) as ClovaChatCompletionsStreamToken;
+        case 'signal':
+          return JSON.parse(data) as ClovaChatCompletionsStreamSignal;
+        default:
+          return null;
+      }
+    } catch (err) {
+      console.error(
+        `Failed to parse JSON:\nstring:\n${singleEvent}\ndata:\n${data}`,
+      );
     }
+  }
+
+  private isValidEvent(match: RegExpExecArray | RegExpMatchArray | null) {
+    return match && match.groups;
   }
 }
