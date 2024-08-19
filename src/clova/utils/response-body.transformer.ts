5da39795e8b4afaa90bbd98629e6d94dde73003f
kimyu0218 <serena35@ewhain.net>
2024-08-19 19:09:27 +0900

    fix: 문자열 배열로 오지 않는 경우에 대한 핸들링 추가 [#118]

--- src/clova/utils/response-body.transformer.ts
+++ src/clova/utils/response-body.transformer.ts
@@ -4,6 +4,10 @@ import {
   Synonyms,
 } from '../types';
 
+const HYPHEN: string = '-';
+const LEFT_BRACE: string = '[';
+const BRACE_REGEXP: RegExp = /\[([^\]]+)\]/;
+
 export class ClovaResponseBodyTransformer {
   static transformIntoResult(
     body: ClovaChatCompletionsResponseBody,
@@ -48,16 +52,52 @@ export class ClovaResponseBodyTransformer {
   }
 
   private static handleUnexpectedSynonymResult(content: string): Synonyms {
-    if (content.includes('-')) {
-      return this.parseSynonymResultWithHyphen(content);
+    console.debug(content);
+    try {
+      if (content.includes(HYPHEN) && content.includes(LEFT_BRACE)) {
+        return this.parseSynonymResultWithHyphenAndBrace(content);
+      }
+      if (content.includes(HYPHEN)) {
+        return this.parseSynonymResultWithHyphen(content);
+      }
+      if (content.includes(LEFT_BRACE)) {
+        return this.parseSynonymResultWithoutQuotationMark(content);
+      }
+      throw Error(content);
+    } catch (err) {
+      throw Error(`Failed to parse synonyms:\n ${err.message}`);
     }
-    throw Error(`Failed to parse synonyms:\n, ${content}`);
+  }
+
+  private static parseSynonymResultWithHyphenAndBrace(
+    content: string,
+  ): string[] {
+    const match: RegExpMatchArray = content.match(BRACE_REGEXP);
+    if (match) {
+      try {
+        return JSON.parse(match[0].slice(1, -1));
+      } catch (err) {
+        return this.handleUnexpectedSynonymResult(match[0]);
+      }
+    }
+    throw Error(content);
   }
 
   private static parseSynonymResultWithHyphen(content: string): string[] {
     return content
       .trim()
-      .split('\n')
-      .map((item) => item.trim().replace(/^- /, ''));
+      .split(/(\n|,)/g)
+      .map((item) => item.trim().replace(/(- |,)/, ''))
+      .filter((item) => item);
+  }
+
+  private static parseSynonymResultWithoutQuotationMark(
+    content: string,
+  ): string[] {
+    return content
+      .replace(/(\[|\])/g, '')
+      .split(',')
+      .map((item) => item.trim())
+      .filter((item) => item);
   }
 }
