b83a84337067355d4c5799de75e6dfcf7aed20db
kimyu0218 <serena35@ewhain.net>
2024-08-20 03:22:13 +0900

    feat: ClovaResponseBodyParser 내보내기

--- src/clova/utils/index.ts
+++ src/clova/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './clova-event.parser';
 export * from './request-api';
 export * from './request-body.transformer';
+export * from './response-body.parser';
 export * from './response-body.transformer';
