0cf08c763fd67368a0cf3afa96d4ed06e558444e
kimyu0218 <serena35@ewhain.net>
2024-08-20 03:14:32 +0900

    feat: ClovaResponseBodyParser 정의 [#124]
    - 예상치 못한 문자열을 문자열 배열로 파싱
    - 다른 clova 요청에서도 재활용할 수 있도록 별도의 파일로 분리

---
+++ src/clova/utils/response-body.parser.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+
+const HYPHEN: string = '-';
+const LEFT_BRACE: string = '[';
+const QUOTATION_MARK: RegExp = /("|')/;
+const BRACE_REGEXP: RegExp = /\[([^\]]+)\]/;
+
+@Injectable()
+export class ClovaResponseBodyParser {
+  parseUnexpectedStringArray(input: string): string[] {
+    try {
+      if (input.includes(HYPHEN) && input.includes(LEFT_BRACE)) {
+        return this.parseStringArrayWithHyphenAndBrace(input);
+      }
+      if (input.includes(HYPHEN)) {
+        return this.parseStringArrayWithHyphen(input);
+      }
+      if (!QUOTATION_MARK.test(input)) {
+        return this.parseStringArrayWithoutQuotationMark(input);
+      }
+      throw Error(input);
+    } catch (err) {
+      throw Error(`Failed to parse synonyms:\n ${err.message}`);
+    }
+  }
+
+  private parseStringArrayWithHyphenAndBrace(input: string): string[] {
+    const match: RegExpMatchArray = input.match(BRACE_REGEXP);
+    if (match) {
+      const matchString = match[0].slice(1, -1);
+      try {
+        return JSON.parse(matchString);
+      } catch (err) {
+        return this.parseUnexpectedStringArray(matchString);
+      }
+    }
+    throw Error(input);
+  }
+
+  private parseStringArrayWithHyphen(input: string): string[] {
+    return input
+      .trim()
+      .split(/(\n|,)/g)
+      .map((item: string) => item.trim().replace(/(-\s|,)/, ''))
+      .filter(Boolean);
+  }
+
+  private parseStringArrayWithoutQuotationMark(input: string): string[] {
+    return input
+      .replace(/(\[|\])/g, '')
+      .split(',')
+      .map((item: string) => item.trim())
+      .filter(Boolean);
+  }
+}
