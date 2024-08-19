10b190234156baec857ba5787d88a9bf685d6274
ttobe <ahdrmfgur12@gmail.com>
2024-08-19 17:27:15 +0900

    fix: 공백 요청 제외 [#117]

--- views/javascript/index.js
+++ views/javascript/index.js
@@ -53,11 +53,11 @@ feedbackFloatingBtn.addEventListener('click', () => {
 });
 
 feedbackBtn.addEventListener('click', () => {
-  const text = document.getElementById('textarea').value;
+  const text = document.getElementById('textarea').value.trim();
   if (text.length > 3000) {
     alertPopup.pop('3000자 이상이면 피드백할 수 없습니다.');
   } else if (text.length < 300) {
-    alertPopup.pop('300자 미만이면 피드백할 수 없습니다.');
+    alertPopup.pop('앞 뒤 공백 제외 300자 미만이면 피드백할 수 없습니다.');
   } else {
     feedbackPopup.show();
   }
