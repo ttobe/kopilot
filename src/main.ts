71716cebf62457c6e4a839cb71c1e63ce2b286eb
kimyu0218 <serena35@ewhain.net>
2024-08-21 05:09:05 +0900

    refactor: Handlebars 설정 분리

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
-import * as Handlebars from 'express-handlebars';
 import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { setupHandlebars } from './common/config/handlebars';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app: NestExpressApplication =
+    await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'views'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  const root: string = join(__dirname, '..');
+  app.useStaticAssets(join(root, 'views'));
+  app.setBaseViewsDir(join(root, 'views'));
 
-  const handlebars = Handlebars.create({
-    partialsDir: join(__dirname, '..', 'views/partials'),
-    extname: '.hbs',
-    helpers: {
-      parseJson: (jsonString) => JSON.parse(jsonString.replace(/\n|\r\n/g, '')),
-    },
-  });
-  app.engine('hbs', handlebars.engine);
+  setupHandlebars(app, root);
 
   await app.listen(3000);
 }
